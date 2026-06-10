3a950d115bb435913d98735e63c4bc3ac3da6c61
Anders Refsdal Olsen <anders@refsdal.io>
2026-06-10 23:40:30 +0200

    ci: add exception for conventional commits for dependabot
    This is due to large body sizes when dependabot makes large updates

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,11 @@
-export default {
+import type { UserConfig } from "@commitlint/types";
+
+const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  ignores: [
+    (commit) =>
+      commit.includes("dependabot") || commit.startsWith("chore(deps):"),
+  ],
 };
+
+export default config;
