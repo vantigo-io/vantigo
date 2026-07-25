c21e02a14762e315533455ff4cef4006d19623e3
Anders Refsdal Olsen <anders@refsdal.io>
2026-07-25 11:18:44 +0200

    chore(customers): add tanstack router for customers frontend

---
+++ apps/customers/frontend/src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+const RootLayout = () => (
+    <>
+        <div className="p-2 flex gap-2">
+            <Link to="/" className="[&.active]:font-bold">
+                Home
+            </Link>{' '}
+            <Link to="/sign-in" className="[&.active]:font-bold">
+                Sign in
+            </Link>
+        </div>
+        <hr />
+        <Outlet />
+        <TanStackRouterDevtools />
+    </>
+)
+
+export const Route = createRootRoute({ component: RootLayout })
--- apps/customers/frontend/vite.config.ts
+++ apps/customers/frontend/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tanstackRouter from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        tanstackRouter({
+            target: 'react',
+            autoCodeSplitting: true,
+            routesDirectory: "src/routes",
+            generatedRouteTree: "src/routeTree.gen.ts",
+            quoteStyle: "single",
+            routeFileIgnorePrefix: "-",
+        }),
+        react(),
+    ],
     server: {
         port: 10011,
         proxy: {
